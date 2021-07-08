c777d02e2086e7a8984dc5bba42f9b1a394ab686
doug <doug@atsg.net>
2021-07-08 15:45:39 -0700

    Initial push code

---
+++ tests/test_uccewrapper.py
@@ -0,0 +1,16 @@
+#tests/test_uccewrapper.py
+
+from uccewrapper import ucceAPI
+
+
+def test_ucce_domain():
+
+    """Tests an API call to get UCCE Domain"""
+
+    ucce_api_instance = ucceAPI('<ip/domain>','<username@domain>', '<password>')
+
+    response = ucce_api_instance.activedirectorydomain
+
+
+    assert isinstance(response,dict)
+    assert response['1'] is not None, "This should be the first associated domain"
---
+++ uccewrapper/api.py
@@ -0,0 +1,2 @@
+class ucceAPI(object):
+    pass
